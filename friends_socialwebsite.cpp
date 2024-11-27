1ff5bd2e44afb48e875e16644c268ef55777d337
bansilal-dot <mpg0012360@gmail.com>
2024-11-27 19:21:45 +0530

    Update and rename 3_a.cpp to friends_socialwebsite.cpp

--- friends_socialwebsite.cpp
+++ friends_socialwebsite.cpp
@@ -5,12 +5,11 @@
 
 using namespace std;
 
-// Structure to store user data
 struct User {
     string name;
-    string dob;  // Date of birth in YYYY-MM-DD format
+    string dob;  
     int num_comments;
-    vector<int> friends;  // List of friend indices (adjacency list)
+    vector<int> friends;  
 
     User(string name, string dob, int comments)
         : name(name), dob(dob), num_comments(comments) {}
@@ -21,7 +20,7 @@ private:
     vector<User> users;
 
 public:
-    // Add a user to the network
+   
     void addUser() {
         string name, dob;
         int comments;
@@ -35,13 +34,11 @@ public:
         users.push_back(User(name, dob, comments));
     }
 
-    // Add a friendship (undirected graph)
     void addFriendship() {
         int user1, user2;
         cout << "Enter two user indices (0-based) to add friendship: ";
         cin >> user1 >> user2;
 
-        // Ensure valid indices
         if (user1 >= 0 && user1 < users.size() && user2 >= 0 && user2 < users.size()) {
             users[user1].friends.push_back(user2);
             users[user2].friends.push_back(user1);
@@ -51,12 +48,10 @@ public:
         }
     }
 
-    // 1. Find user with maximum friends
     int maxFriends() {
         int maxFriendsIndex = -1;
         int maxCount = -1;
         for (int i = 0; i < users.size(); ++i) {
-            // Check the size of friends list for each user
             int friendCount = users[i].friends.size();
             if (friendCount > maxCount) {
                 maxCount = friendCount;
@@ -66,7 +61,7 @@ public:
         return maxFriendsIndex;
     }
 
-    // 2. Find user with max and min number of comments
+  
     pair<int, int> maxMinComments() {
         int maxCommentsIndex = -1, minCommentsIndex = -1;
         int maxComments = INT_MIN, minComments = INT_MAX;
@@ -84,16 +79,15 @@ public:
         return {maxCommentsIndex, minCommentsIndex};
     }
 
-    // 3. Find users having birthday in the current month
     vector<int> usersWithBirthdayThisMonth() {
         vector<int> birthdayUsers;
         time_t t = time(0);
         struct tm* now = localtime(&t);
 
-        int currentMonth = now->tm_mon + 1;  // Months are 0-based, so add 1
+        int currentMonth = now->tm_mon + 1; 
         
         for (int i = 0; i < users.size(); ++i) {
-            // Extract month from the date of birth (format YYYY-MM-DD)
+          
             int birthMonth = stoi(users[i].dob.substr(5, 2));
             
             if (birthMonth == currentMonth) {
@@ -103,7 +97,6 @@ public:
         return birthdayUsers;
     }
 
-    // Utility function to display the user details
     void displayUserDetails(int index) {
         if (index >= 0 && index < users.size()) {
             cout << "Name: " << users[index].name << endl;
@@ -117,7 +110,6 @@ public:
         }
     }
 
-    // Utility function to display the list of all users
     void displayAllUsers() {
         for (int i = 0; i < users.size(); ++i) {
             displayUserDetails(i);
@@ -133,7 +125,6 @@ int main() {
     cout << "Enter the number of users to add: ";
     cin >> numUsers;
 
-    // Add users to the network
     for (int i = 0; i < numUsers; ++i) {
         sn.addUser();
     }
@@ -142,22 +133,19 @@ int main() {
     cout << "Enter the number of friendships to add: ";
     cin >> numFriendships;
 
-    // Add friendships
     for (int i = 0; i < numFriendships; ++i) {
         sn.addFriendship();
     }
     cout<<"--------------------------------------------"<<endl;
-    // Task 1: Find user with maximum friends
     int maxFriendIndex = sn.maxFriends();
     cout << "User with maximum friends: " << endl;
     if (maxFriendIndex != -1) {
-        sn.displayUserDetails(maxFriendIndex);  // Display details of the user with the maximum friends
+        sn.displayUserDetails(maxFriendIndex);  
     } else {
         cout << "No users found!" << endl;
     }
     cout<<"--------------------------------------------"<<endl;
 
-    // Task 2: Find user with maximum and minimum comments
     pair<int, int> result = sn.maxMinComments();
     int maxCommentUser = result.first;
     int minCommentUser = result.second;
@@ -170,8 +158,6 @@ int main() {
     cout << "User with minimum comments: " << endl;
     sn.displayUserDetails(minCommentUser);
     cout<<"--------------------------------------------"<<endl;
-
-    // Task 3: Find users with birthday in the current month
     cout<<"--------------------------------------------"<<endl;
 
     vector<int> birthdayUsers = sn.usersWithBirthdayThisMonth();
