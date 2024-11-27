c3ef5c3a63e88f9646c0d39603a9d91b69b3d448
bansilal-dot <mpg0012360@gmail.com>
2024-11-27 19:17:42 +0530

    Update and rename 2.cpp to inorder.cpp

--- inorder.cpp
+++ inorder.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <stack>
-#include <cctype> // For isdigit()
+#include <cctype> 
 using namespace std;
 
-// Define a tree node
 struct TreeNode {
     char value;
     TreeNode* left;
@@ -11,16 +10,14 @@ struct TreeNode {
     TreeNode(char val) : value(val), left(nullptr), right(nullptr) {}
 };
 
-// Function to construct the expression tree from postfix
 TreeNode* constructExpressionTree(const string& postfix) {
     stack<TreeNode*> st;
 
     for (char ch : postfix) {
-        // If operand, create a node and push onto the stack
         if (isalnum(ch)) {
             st.push(new TreeNode(ch));
         }
-        // If operator, pop two nodes, create a new node, and push back onto stack
+      
         else {
             TreeNode* right = st.top(); st.pop();
             TreeNode* left = st.top(); st.pop();
@@ -30,25 +27,23 @@ TreeNode* constructExpressionTree(const string& postfix) {
             st.push(newNode);
         }
     }
-    // The remaining node in the stack is the root
+
     return st.top();
 }
-
-// Non-recursive inorder traversal
 void inorderTraversal(TreeNode* root) {
     stack<TreeNode*> st;
     TreeNode* current = root;
 
     while (!st.empty() || current) {
-        // Reach the leftmost node
+       
         while (current) {
             st.push(current);
             current = current->left;
         }
-        // Visit the node
+     
         current = st.top(); st.pop();
         cout << current->value << " ";
-        // Visit the right subtree
+      
         current = current->right;
     }
 }
@@ -56,14 +51,12 @@ void inorderTraversal(TreeNode* root) {
 int main() {
     string postfix;
 
-    // Taking input from the user
     cout << "Enter a postfix expression: ";
     cin >> postfix;
 
-    // Construct the expression tree
     TreeNode* root = constructExpressionTree(postfix);
 
-    // Perform non-recursive inorder traversal
+
     cout << "Inorder Traversal of Expression Tree: ";
     inorderTraversal(root);
     cout << endl;
